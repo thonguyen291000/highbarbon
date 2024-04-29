9a5e4ae193b3f3a21c4435de258c8f71ba7044b8
Tho Nguyen <tho.nguyen@employmenthero.com>
2024-04-29 13:43:51 +0700

    add location identification when tracking

--- src/controller/track.controller.ts
+++ src/controller/track.controller.ts
@@ -3,13 +3,24 @@ import { Request, Response } from "express";
 import Track from "../models/tracks";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
+const geoip = require("geoip-lite");
 dotenv.config();
 
 export const createTrackEvent = async (req: Request, res: Response) => {
+  let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  if (ip === "::1" || ip?.includes("127.0.0.1")) {
+    ip = "8.8.8.8";
+  }
+
+  const geo = geoip.lookup(ip);
+
   const track = new Track({
     _id: new mongoose.Types.ObjectId().toString(),
     event_name: req.body.event_name,
-    metadata: req.body.metadata,
+    metadata: {
+      ...req.body.metadata,
+      location: geo,
+    },
   });
 
   try {
